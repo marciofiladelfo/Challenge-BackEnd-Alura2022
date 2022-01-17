5f81d62871312738f5195a73eb023b3bab6d46be
marciofiladelfo <marcio.kabana@gmail.com>
2022-01-17 12:55:50 -0300

    first commit

---
+++ src/test/java/com/alura/challengebackend/ChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.challengebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
